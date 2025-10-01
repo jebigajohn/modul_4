c5120415fe700a921dd97f3fc33dc736db4c5fe8
Johnstb <jstaub8596@gmail.com>
2025-10-01 12:25:20 +0200

    commit

---
+++ 03_supabase-rezeptApp/src/functions/getProducts.ts
@@ -0,0 +1,98 @@
+import type { IProduct } from '../interfaces/IProduct'
+import supabase from '../utils/supabase'
+
+export async function getProducts_store(): Promise<IProduct[]> {
+  const { data: products, error } = await supabase
+    .from('products')
+    .select('*')
+    .textSearch('title', 'laptop | maus')
+  if (error) {
+    console.error(error)
+  }
+
+  return products as IProduct[]
+}
+
+// # selectQueries Filter Supabase Methoden
+
+// Filtern mit eq(), lt(), gt(), like(), between(), in()
+// ! lt = lower then => kleiner als
+// ! gt = greater then => größer als
+// ? 1 SQL CODE
+// ? SELECT * FROM products WHERE quality = "High"
+// * REACT CODE
+// * const { data, error } = await supabase
+// *  .from('products')
+// *  .select('*')
+// *  .eq('quality', 'High')
+
+// ? 2 Quality ist nicht gleich Low
+// * const { data: products, error } = await supabase
+// *  .from('products')
+// *  .select('*')
+// *  .neq('quality', 'Low')
+
+// ? 3 SQL CODE
+// ? SELECT * FROM products WHERE quality = "High" and price < 500
+// * const { data: products, error } = await supabase
+// *   .from('products')
+// *   .select('*')
+// *   .eq('quality', 'High')
+// *   .lt('price', 500)
+
+// ? 4 SQL CODE
+// ? SELECT * FROM products WHERE price >= 50 AND price <= 200
+// *  const { data: products, error } = await supabase
+// *   .from('products')
+// *   .select('*')
+// *   .gte('price', 50)
+// *   .lte('price', 200)
+
+// * const { data: products, error } = await supabase
+// *  .from('products')
+// *  .select('*')
+// *  .eq('quality', 'High')
+// *  .gte('price', 50)
+// *  .lte('price', 200)
+
+// ! Alternative
+// * .from('products')
+// *  .select('price')
+// *  .range(50, 200)
+
+// ? 5 SQL CODE
+// ? SELECT * FROM products WHERE quality IN ("High", Medium
+// * .from('products')
+// * .select('*')
+// * .in("quality", ["High", "Medium"])
+
+// ? 6 SQL CODE
+// ? SELECT * FROM products WHERE quality = "High" OR price < 50
+// * .from('products')
+// * .select('*')
+// * .or("quality.eq.High, price.lt.50")
+
+// ? 7 SQL CODE
+// ? SELECT * FROM products WHERE price >= 50 AND quality = "High"
+// .from('products')
+// .select('*')
+// .filter("price", "gte", 50)
+// .filter("quality", "eq", "High")
+
+// ? 8 SQL CODE
+// ? SELECT * FROM products WHERE title LIKE "%laptop%"
+// .from('products')
+// .select('*')
+// .ilike("title", "%laptop%")
+
+// ? 9 Nur zwei spalten anzeigen Title und Price
+// const { data: products, error } = await supabase
+//   .from('products')
+//   .select('title, price')
+//   .eq('quality', 'High')
+
+// ? Just to know
+// const { data: products, error } = await supabase
+//   .from('products')
+//   .select('*')
+//   .textSearch('title', 'laptop | maus')
---
+++ 03_supabase-rezeptApp/src/functions/getProducts_v2.ts
@@ -0,0 +1,44 @@
+import { data } from 'react-router'
+import type { IProduct } from '../interfaces/IProduct'
+import supabase from '../utils/supabase'
+import type { ICart } from '../interfaces/ICart'
+import type { ICategory } from '../interfaces/ICategory'
+
+// # Produkte + Kategorien anzeigen
+
+export async function getProductAndCategory(): Promise<IProduct[]> {
+  const { data: products, error } = await supabase
+    .from('products')
+    .select('id, title, price, quality,category:categories(category_name)')
+  if (error) {
+    console.error(error)
+  }
+  console.log(products)
+  return products as IProduct[]
+}
+
+// # Cart_Items mit Produkten + Kategorien
+
+export async function getCart(): Promise<ICart> {
+  const { data: cart, error } = await supabase
+    .from('cart_items')
+    .select(
+      'id, cart_id, quantity, products:products(id,title,price,quality,categories(category_name))'
+    )
+  if (error) {
+    console.error(error)
+  }
+  console.log(cart)
+  return cart as unknown as ICart
+}
+
+export async function getCategory(): Promise<ICategory> {
+  const { data: category, error } = await supabase
+    .from('categories')
+    .select('category_name, products_table:products(*)')
+  if (error) {
+    console.error(error)
+  }
+  console.log(category)
+  return category as unknown as ICategory
+}
